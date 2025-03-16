fcaef39b3fc80a6de0caf2225ef4bf557d5c63d0
Amit Nandi <amitnandileo@gmail.com>
2025-03-16 17:55:58 +0530

    Add redirect file to redirector branch

---
+++ next.config.mjs
@@ -0,0 +1,19 @@
+/** @type {import('next').NextConfig} */
+
+const isProd = process.env.NODE_ENV === 'production';
+
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    unoptimized: true,
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**", // Allows images from any domain
+      },
+    ],
+  },
+
+};
+
+export default nextConfig;
